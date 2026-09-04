5b51b4053b87c9b7edded193caeb1d2f862d71c0
luissierraemermedica-dotcom <luis.sierra.emermedica@gmail.com>
2026-09-04 13:16:25 -0500

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,7 @@
+/*
+   Consulta para obtener la cantidad total de clientes.
+   COUNT(*) cuenta todos los registros de la tabla customers.
+   El resultado se muestra con el alias customers_count.
+*/
+SELECT COUNT(*) AS customers_count
+FROM customers;
